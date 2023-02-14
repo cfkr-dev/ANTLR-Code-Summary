eb5666ced8f1e3ef09379c721c38c2ec51619193
Alberto Pérez <56350482+C0nf1cker@users.noreply.github.com>
2023-02-14 13:06:56 +0100

    Modificado fichero de constantes

--- src/Constants.java
+++ src/Constants.java
@@ -1,14 +1,12 @@
-public static class Constants {
-  
-  private static String TEST_PATH = "./test/";
+public class Constants {
+
+  // RUTA RAIZ PARA TEST
+  private static final String TEST_PATH = "./test/";
   
   // RUTA PARA FICHEROS DE ENTRADA
-  private static String TEST_IN_PATH = "./test/in/";
+  private static final String TEST_IN_PATH = "./test/in/";
   
   // RUTA PARA FICHEROS DE SALIDA
-  private static String TEST_OUT_PATH = "./test/out/";
-
-  // ESTO ES UN CAMBIO EN FORMA DE COMENTARIO
-  // OTRO CAMBIO MAS
+  private static final String TEST_OUT_PATH = "./test/out/";
 
 }
