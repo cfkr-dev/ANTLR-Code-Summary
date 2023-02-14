29b111bc1cbabd6b0d8e4a6ebc002557e4a3b0ce
Alberto Pérez <56350482+C0nf1cker@users.noreply.github.com>
2023-02-14 12:29:40 +0100

    Create Constants.java

---
+++ src/Constants.java
@@ -0,0 +1,11 @@
+public static class Constants {
+  
+  private static String TEST_PATH = "./test/";
+  
+  // RUTA PARA FICHEROS DE ENTRADA
+  private static String TEST_IN_PATH = "./test/in/";
+  
+  // RUTA PARA FICHEROS DE SALIDA
+  private static String TEST_OUT_PATH = "./test/out/";
+
+}
