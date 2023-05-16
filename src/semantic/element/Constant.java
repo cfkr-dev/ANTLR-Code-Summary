401fdd1b695d32e7128a50501504d8ec6b649960
cfkr-dev <albertoperezperez1@hotmail.com>
2023-05-16 18:07:57 +0200

    Añadido creaccion de constantes

---
+++ src/semantic/element/Constant.java
@@ -0,0 +1,36 @@
+package semantic.element;
+
+import semantic.element.sentence.literal.literal_master.Literal;
+import semantic.element.sentence.sentence_interface.AssignableElement;
+import semantic.element_interfaces.ProgrammableElement;
+import semantic.element_master.MasterProgramElement;
+import semantic.enums.Element;
+
+public class Constant extends MasterProgramElement implements AssignableElement {
+
+    private Literal value;
+
+    public Constant(String name, Literal value, ProgrammableElement context) {
+        this.type = value.getType();
+        this.elementType = Element.CONSTANT;
+        this.name = name;
+        this.context = context;
+        this.value = value;
+    }
+
+    public AssignableElement getValueTarget() {
+        return this.value;
+    }
+
+    @Override
+    public String getLiteralValue() {
+        return this.value.getLiteralValue();
+    }
+
+    @Override
+    public String toHTML() {
+        return null;
+    }
+
+
+}
